0347c717debb9b1dd05b44cbb2262ff6c9363093
Viraj <viraj@mahitechllc.com>
2022-05-31 14:29:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiverapp/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.example.broadcastreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.i("Broadcast receiver", "Msg is received");
+        Toast.makeText(context, "Broadcast Message is received", Toast.LENGTH_SHORT).show();
+    }
+}
